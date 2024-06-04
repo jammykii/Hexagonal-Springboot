d95f75c10f06aa451deeb24870bd4a76579d3cf0
gimjuncheol <gimjuncheol839@idrsystems.co.kr>
2024-06-04 18:33:05 +0900

    first

---
+++ src/main/kotlin/com/example/hexagonalspringboot/HexagonalSpringbootApplication.kt
@@ -0,0 +1,12 @@
+package com.example.hexagonalspringboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HexagonalSpringbootApplication
+
+fun main(args: Array<String>) {
+    /** first **/
+    runApplication<HexagonalSpringbootApplication>(*args)
+}
